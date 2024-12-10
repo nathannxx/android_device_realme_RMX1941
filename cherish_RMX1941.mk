faf6dd2c04c29ba79792d4402bd0f09ae9a40b60
Nathan Andrew <nathannxx@proton.me>
2024-12-11 00:59:45 +0700

    CherishOS¹
    Signed-off-by: Nathan Andrew <nathannxx@proton.me>

--- cherish_RMX1941.mk
+++ cherish_RMX1941.mk
@@ -56,4 +56,5 @@ PRODUCT_BUILD_PROP_OVERRIDES := \
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+    ro.build.fingerprint=$(BUILD_FINGERPRINT) \
+    ro.cherish.maintainer=nathan
