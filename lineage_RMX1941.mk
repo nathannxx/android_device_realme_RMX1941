7ab21ba9a9c53dea724d3614f21ba1b7aa44da28
Nathan Andrew <nathannxx@proton.me>
2024-12-11 02:18:38 +0700

    prop: use three buttons navigation as default
    Signed-off-by: Nathan Andrew <nathannxx@proton.me>

--- lineage_RMX1941.mk
+++ lineage_RMX1941.mk
@@ -62,4 +62,5 @@ PRODUCT_BUILD_PROP_OVERRIDES := \
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+    ro.build.fingerprint=$(BUILD_FINGERPRINT) \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.threebutton
